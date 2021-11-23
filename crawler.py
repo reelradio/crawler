2dd341bae393bb47109befedab0578323c033a0d
Zed Chance <zchance@csus.edu>
2021-11-22 17:24:47 -0800

    Overload constructor with kwargs
    New args:
    - 'max_depth': max recursion depth
    - 'indent_depth': print output with indentation
    - 'ignore_prefixes': don't crawl any links with these prefixes
    - 'ignore_suffixes': don't crawl any links with these suffixes

--- crawler.py
+++ crawler.py
@@ -1,4 +1,5 @@
 # a web page crawler with different strategies
+import math
 
 import requests
 from bs4 import BeautifulSoup
@@ -17,13 +18,20 @@ class PrinterStrategy(CrawlerStrategy):
 
 
 class Crawler:
-    def __init__(self, url, strategy, relative_urls=True, explore=True):
+    def __init__(self, url, strategy: CrawlerStrategy, **kwargs):
         self.url = url
         if not isinstance(strategy, CrawlerStrategy):
             raise TypeError("strategy must be implementation of CrawlerStrategy")
         self.strategy = strategy
-        self.relative_urls = relative_urls
-        self.explore = explore
+        self.relative_urls = kwargs.get('relative_urls', True)
+        self.explore = kwargs.get('explore', True)
+        self.depth = 0
+        self.max_depth = kwargs.get('max_depth', math.inf)
+        self.indent_depth = kwargs.get('indent_depth', False)
+        self.ignore_prefixes = kwargs.get('ignore_prefixes', ('mailto',))
+        self.ignore_suffixes = kwargs.get('ignore_suffixes', ('pdf',))
+        if self.relative_urls:
+            self.ignore_prefixes = self.ignore_prefixes + ('http',)
 
     def crawl(self):
         visited = {}
@@ -43,10 +51,10 @@ class Crawler:
                 return
             soup = BeautifulSoup(page.content, 'html.parser')
             visited[current_url] = page.status_code
-            print(visited[current_url], current_url)
 
             # perform strategy
             if page.status_code == 200:
+                print(" " * self.depth if self.indent_depth else "", self.depth, visited[current_url], current_url)
                 self.strategy.execute(soup)
             else:
                 return
@@ -55,15 +63,20 @@ class Crawler:
             if not self.explore:
                 return
             for anchor in soup.find_all('a'):
-                if anchor is None:
+                if anchor is None or anchor in visited:
                     continue
                 next_url = anchor.get('href')
-                if next_url is None:
+                if next_url is None or next_url.startswith(self.ignore_prefixes) or next_url.endswith(self.ignore_suffixes):
                     continue
                 if not next_url.startswith('/') and self.relative_urls:
                     next_url = '/' + next_url
 
-                _crawl(next_url)
+                if self.depth < self.max_depth:
+                    self.depth += 1
+                    _crawl(next_url)
+                    self.depth -= 1
+                else:
+                    break
 
         # crawl the site
         _crawl(self.url)
